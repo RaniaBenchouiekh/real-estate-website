2278cfc0ba28c3f245edc13e38f515e183b8781c
rania benchouiekh <raniabenchouiekh96@gmail.com>
2021-03-09 17:21:43 +0100

    slider-styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import GlobalStyle from './globalStyles';
-
+import {SliderData} from './data/SliderData'
 
 function App() {
   return (
     <>
-    <GlobalStyle />
-    <Navbar />
+      <GlobalStyle />
+      <Navbar />
+      <Hero slides={SliderData}/>
     </>
   );
 }
